d46f6603b18ac5e17b0d7262746122b035071fbb
devin <120673199+znxdv@users.noreply.github.com>
2024-06-22 23:16:57 +0700

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDOFwy7eEHCv9I8U8Bced0Jp0fMQbHJF8Y",
+  authDomain: "terrapure-a81e0.firebaseapp.com",
+  projectId: "terrapure-a81e0",
+  storageBucket: "terrapure-a81e0.appspot.com",
+  messagingSenderId: "79172956936",
+  appId: "1:79172956936:web:cdd3ea3db14d425b9ab2bd",
+  measurementId: "G-S7H8MSF7X0",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
